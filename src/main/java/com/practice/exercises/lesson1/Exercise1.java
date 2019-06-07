bc14ecf681be254a954cff31b6c98327ecb1903e
Mariya Barkovskaya <mariya.barkovskaya@nosota.com>
2019-06-07 13:18:19 +0300

    ex 1, 2, 3, 4 (ex.4 without second approach)

--- src/main/java/com/practice/exercises/lesson1/Exercise1.java
+++ src/main/java/com/practice/exercises/lesson1/Exercise1.java
@@ -1,6 +1,7 @@
 package com.practice.exercises.lesson1;
 
 public class Exercise1 {
+
     public static void main(String[] args) {
         char ch = 70;
         int n = ch + 50;
@@ -17,16 +18,22 @@ public class Exercise1 {
         isEven(101);
         System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" + "\n");
 
-        convert(3);
+        convert(1);
         System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" + "\n");
+
+        System.out.println(sum(-2, 2));
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" + "\n");
+
+//        System.out.println(sum2(2, 2));
     }
 
     private static void isEven(int n) {
         String res = "";
-        if (n % 2 >= 0) {
+        if (n % 2 == 0) {
             res = "n is even";
+        } else {
+            res = "n is odd";
         }
-        res = "n is odd";
         System.out.println(res);
     }
 
@@ -34,4 +41,45 @@ public class Exercise1 {
         String s = (n == 1) ? "one" : (n == 2) ? "two" : "three";
         System.out.println(s);
     }
+
+    private static int sum(int a, int b) {
+        int sum = 0;
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("a or b less than zero");
+
+        }
+        while (sum < a) {
+            sum++;
+        }
+        int counter = 0;
+        while (counter < b) {
+            counter++;
+            sum++;
+        }
+        return sum;
+    }
+
+//    private static int sum2(int a, int b) {
+//        int sum = 0;
+//        if (a < 0 || b < 0) {
+//            throw new IllegalArgumentException("a or b less than zero");
+//
+//        }
+//        sum = sum(a, b, 0, 0);
+//        return sum;
+//    }
+//
+//    private static int sum(int a, int b, int x, int cnt) {
+//        if (x < a) {
+//            x += 1;
+//            sum(a, b, x, cnt);
+//        } else if (cnt < b) {
+//            cnt++;
+//            x += 1;
+//            sum(a, b, x, cnt);
+//        }
+//
+//        return x;
+//    }
+
 }
